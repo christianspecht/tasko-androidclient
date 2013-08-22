dc98841f9615ed7f4c196040479ffe9c40279c3c
Christian Specht <christi@nspecht.de>
2013-08-22 23:57:48 +0200

    initial commit

---
+++ gen/de/christianspecht/tasko/androidclient/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.christianspecht.tasko.androidclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
