35810dc7e1daecbec9f3d23354e9bbf0c62be215
Christian Specht <christi@nspecht.de>
2015-05-27 18:13:03 +0200

    disable menu in login screen

--- app/src/main/java/de/christianspecht/tasko/androidclient/LoginActivity.java
+++ app/src/main/java/de/christianspecht/tasko/androidclient/LoginActivity.java
@@ -17,8 +17,7 @@ public class LoginActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_login, menu);
+		// disable menu
 		return true;
 	}
 
