e4f3c5d09056143b83ec3436ee559c08bbc8ba91
Christian Specht <christi@nspecht.de>
2015-05-27 18:46:44 +0200

    helper class to load and save preferences

---
+++ app/src/main/java/de/christianspecht/tasko/androidclient/Prefs.java
@@ -0,0 +1,33 @@
+package de.christianspecht.tasko.androidclient;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Load and save preferences
+ */
+public class Prefs {
+
+	private SharedPreferences pref;
+
+	public Prefs(Context context) {
+		this.pref = PreferenceManager.getDefaultSharedPreferences(context);
+	}
+
+	/**
+	 * Loads auth token from the preferences
+	 * @return The token
+	 */
+	public String getAuthToken() {
+		return this.pref.getString("AuthToken", "");
+	}
+
+	/**
+	 * Saves auth token in the preferences
+	 * @param token The token
+	 */
+	public void setAuthToken(String token) {
+		this.pref.edit().putString("AuthToken", token).commit();
+	}
+}
