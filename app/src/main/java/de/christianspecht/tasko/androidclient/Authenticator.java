8b7a711200ebf1c00b7c72575d0246ad42a275e2
Christian Specht <christi@nspecht.de>
2015-08-26 18:29:32 +0200

    authenticate with Basic Auth to get session token

--- app/src/main/java/de/christianspecht/tasko/androidclient/Authenticator.java
+++ app/src/main/java/de/christianspecht/tasko/androidclient/Authenticator.java
@@ -2,6 +2,14 @@ package de.christianspecht.tasko.androidclient;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Base64;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Helper class for authentication
@@ -34,6 +42,36 @@ public class Authenticator extends AsyncTask<String, Void, String> {
 	@Override
 	protected String doInBackground(String... urls){
 
+		HttpURLConnection urlConnection = null;
+		String json = "";
+
+		try {
+			URL url = new URL(urls[0]);
+			urlConnection = (HttpURLConnection)url.openConnection();
+			urlConnection.setRequestMethod("GET");
+
+			String auth1 = this.user + ":" + this.pass;
+			String auth2 = "Basic " + new String(Base64.encode(auth1.getBytes(), Base64.NO_WRAP));
+			urlConnection.setRequestProperty("Authorization", auth2);
+			urlConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			StringBuilder result = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				result.append(line);
+			}
+
+			json = result.toString();
+
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally {
+			urlConnection.disconnect();
+		}
+
+		// TODO: get token from JSON
 		if (this.user.equals("test")){
 			return "token";
 		}
